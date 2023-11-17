d00b8b89fd1d9e8af8aa390672002a12c9387731
haturusinghe <s.haturusinghe99@gmail.com>
2023-11-17 13:26:37 +0530

    working controller

--- Controllers/ProjectController.cs
+++ Controllers/ProjectController.cs
@@ -4,14 +4,10 @@ using projects_api.Models;
 namespace projects_api.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/projects")]
     public class ProjectController : Controller
     {
-        private List<Project> _projects;
-
-        public ProjectController()
-        {
-            this._projects = new List<Project>
+        private static List<Project> _projects = new List<Project>
         {
             new Project { Id = 1, Name = "Project Breeze", Revenue = 12365.55, IsCompleted = true },
             new Project { Id = 2, Name = "Command Program", Revenue = 4598.5, IsCompleted = false },
@@ -22,12 +18,63 @@ namespace projects_api.Controllers
             new Project { Id = 7, Name = "Dynamic Program", Revenue = 1564.5, IsCompleted = true },
             new Project { Id = 8, Name = "Project ZenSen", Revenue = 12312.75, IsCompleted = false },
         };
+
+
+        [HttpGet]
+        public List<Project> GetAllProjects()
+        {
+            return _projects;
         }
 
         [HttpGet]
-        public IEnumerable<Project> GetAllProjects()
+        [Route("completed")]
+        public List<Project> GetCompletedProjects()
         {
-            return this._projects.ToList();
+            return _projects.FindAll(p => p.IsCompleted == true);
+        }
+
+        [HttpGet]
+        [Route("top")]
+        public List<Project> GetTopProjectsByRevenue()
+        {
+            return _projects.ToList().OrderByDescending(p => p.Revenue).Take(3).ToList();
+        }
+
+        [HttpPost]
+        public Project CreateProject(Project project)
+        {
+            var isValidProject = validateProject(project);
+
+            var maxProjectId = _projects.Max(p => p.Id);
+            project.Id = maxProjectId + 1;
+
+            _projects.Add(project);
+
+            return project;
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public Project DeleteProject([FromRoute]int id)
+        {
+            var project = _projects.Find(p => p.Id == id);
+
+            if (project != null)
+            {
+                _projects.Remove(project);
+            }
+
+            return project;
+        }
+
+        private bool validateProject(Project project)
+        {
+            if (_projects.Any(p => p.Id == project.Id) || _projects.Any(p => p.Name.ToLower().Equals(project.Name.ToLower())))
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
