3ec401cd3096824e197f275b43bce8cd06d1c116
Maneesha-Maduranga <maneeshamaduranga22@gmail.com>
2023-11-20 09:48:29 +0530

    Add getAllProjects Method in ProjectController

---
+++ Controllers/ProjectsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using projects_api.models;
+
+namespace projects_api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProjectsController : Controller
+    {
+
+        private readonly ProjectContext _projectcontext;
+        public ProjectsController(ProjectContext projectContext) 
+        { 
+            _projectcontext = projectContext;
+        }
+
+        [HttpGet]
+        public async  Task<IActionResult> getAllProjects()
+        {
+           var projects =  await _projectcontext.Projects.ToListAsync();
+
+           return Ok(projects);
+        }
+    }
+}
