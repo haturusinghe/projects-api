3eb5356c1675a1980403f718dbde71679b200f79
Chamod Dananjaya <chamodd@embla.asia>
2023-11-21 13:07:40 +0530

    Add project controller with basic route.

---
+++ Controllers/ProjectController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace projects_api.Controllers
+{
+    [ApiController]
+    [Route("api/projects")]
+    public class ProjectController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetProjects()
+        {
+            return Ok("Hello");
+        }
+    }
+}
