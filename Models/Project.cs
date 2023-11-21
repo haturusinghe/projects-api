c52b976794e856f15d2da4fcc0ec35c8375f2fac
Chamod Dananjaya <chamodd@embla.asia>
2023-11-21 13:07:12 +0530

    Add Project class.

---
+++ Models/Project.cs
@@ -0,0 +1,10 @@
+﻿namespace projects_api.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public double Revenue { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
