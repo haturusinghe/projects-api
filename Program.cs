6ea1b881001b6801159a2f7ce99e4dc897ab6565
haturusinghe <s.haturusinghe99@gmail.com>
2023-11-23 15:49:11 +0530

    added descriptive comments

--- Program.cs
+++ Program.cs
@@ -4,12 +4,19 @@ var builder = WebApplication.CreateBuilder(args);
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins"; //this name is arbitary
 
 // Add services to the container.
+//In ASP.NET Core, services such as the DB context must be registered with the dependency injection (DI) container.
+//The container provides the service to controllers.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+//Calls AddCors with a lambda expression.
+//The lambda takes a CorsPolicyBuilder object.
+//Configuration options, such as WithOrigins, are described later in this article.
+//The AddCors method call adds CORS services to the app's service container
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: MyAllowSpecificOrigins,
@@ -36,6 +43,10 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseCors(MyAllowSpecificOrigins); //Adds CORS middleware, need to follow the order and place this here
+//Calls the UseCors extension method and specifies the _myAllowSpecificOrigins CORS policy. UseCors adds the CORS middleware.
+//The call to UseCors must be placed after UseRouting, but before UseAuthorization. For more information, see Middleware order.
+//Enables the _myAllowSpecificOrigins CORS policy for all controller endpoints. 
+
 
 app.UseAuthorization();
 
