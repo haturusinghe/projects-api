dae3e80a2a0317b54da0c07d7d34549d19e02f50
haturusinghe <s.haturusinghe99@gmail.com>
2023-11-23 16:47:23 +0530

    updated project entity to have the properties needed

--- Data/Entities/Project.cs
+++ Data/Entities/Project.cs
@@ -2,5 +2,12 @@
 {
     public class Project
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = "";
+
+        public double Revenue { get; set; }
+
+        public bool IsCompleted { get; set; } = false;
     }
 }
