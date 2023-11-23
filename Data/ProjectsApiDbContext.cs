82f3bcadd1a26e7e20b9000c6214f103336f5c59
haturusinghe <s.haturusinghe99@gmail.com>
2023-11-23 17:45:00 +0530

    added dbContext class

---
+++ Data/ProjectsApiDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectsAPI.Data.Entities;
+
+namespace ProjectsAPI.Data
+{
+    public class ProjectsApiDbContext : DbContext
+    {
+        public DbSet<Project> Projects { get; set; }
+        public ProjectsApiDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
