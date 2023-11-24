a6d4916a99804d235330c40eba7f01577fd02613
Maneesha-Maduranga <maneeshamaduranga22@gmail.com>
2023-11-24 09:33:49 +0530

    Add Project Context

---
+++ models/ProjectContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Projects_api.models
+{
+    public class ProjectContext : DbContext
+    {
+        public ProjectContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Project> Projects { get; set; } = null!;
+    }
+}
