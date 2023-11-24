2b8467a750c8acdb17e39c8068e67c0dcfcf8529
haturusinghe <s.haturusinghe99@gmail.com>
2023-11-24 13:24:29 +0530

    create the implementation of the unit of work

---
+++ Data/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using ProjectsAPI.Repositories;
+using ProjectsAPI.Repositories.Interfaces;
+
+namespace ProjectsAPI.Data
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly ProjectsApiDbContext _context;
+        private readonly ILogger _logger;
+        public IProjectRepository Projects { get; private set; }
+
+        public UnitOfWork(ProjectsApiDbContext context, ILogger logger)
+        {
+            _context = context;
+            _logger = logger;
+            
+            Projects = new ProjectRepository(_context, _logger);
+        }
+        
+
+
+        public async Task CompleteAsync()
+        {
+            await _context.SaveChangesAsync(); 
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
