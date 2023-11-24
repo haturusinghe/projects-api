348a604b3724a573267b51c059f5130750c58451
haturusinghe <s.haturusinghe99@gmail.com>
2023-11-24 13:17:27 +0530

    added methods relavent to project table

--- Repositories/Interfaces/IProjectRepository.cs
+++ Repositories/Interfaces/IProjectRepository.cs
@@ -4,5 +4,7 @@ namespace ProjectsAPI.Repositories.Interfaces
 {
     public interface IProjectRepository : IGenericRepository<Project>
     {
+        Task<IEnumerable<Project>> GetTopByRevenue();
+        Task<IEnumerable<Project>> GetCompleted();
     }
 }
