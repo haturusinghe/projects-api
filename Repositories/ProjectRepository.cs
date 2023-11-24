3d3bc74a87b9e00c9f103d1bc9f1c86c3fd81fbe
haturusinghe <s.haturusinghe99@gmail.com>
2023-11-24 13:18:40 +0530

    added implementation for getTop and getCompleted methods

--- Repositories/ProjectRepository.cs
+++ Repositories/ProjectRepository.cs
@@ -1,4 +1,5 @@
-﻿using ProjectsAPI.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectsAPI.Data;
 using ProjectsAPI.Data.Entities;
 using ProjectsAPI.Repositories.Interfaces;
 
@@ -8,6 +9,32 @@ namespace ProjectsAPI.Repositories
     {
         public ProjectRepository(ProjectsApiDbContext context, ILogger logger) : base(context, logger)
         {
+        }
+
+        public async Task<IEnumerable<Project>> GetCompleted()
+        {
+            try
+            {
+                return await _context.Projects.Where(p => p.IsCompleted).ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed to get completed projects: {ex}");
+                return null;
+            }
+        }
+
+        public async Task<IEnumerable<Project>> GetTopByRevenue()
+        {
+            try
+            {
+                return await _context.Projects.Where(p=> p.Revenue > 0).OrderByDescending(p => p.Revenue).Take(3).ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed to get top projects by revenue: {ex}");
+                return null;
+            }
         }
     }
 }
