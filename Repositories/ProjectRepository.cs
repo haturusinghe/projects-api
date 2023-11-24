73a015893085bd7ed0103cb13ef379567961ba06
haturusinghe <s.haturusinghe99@gmail.com>
2023-11-24 12:52:08 +0530

    created Project repository class

---
+++ Repositories/ProjectRepository.cs
@@ -0,0 +1,13 @@
+﻿using ProjectsAPI.Data;
+using ProjectsAPI.Data.Entities;
+using ProjectsAPI.Repositories.Interfaces;
+
+namespace ProjectsAPI.Repositories
+{
+    public class ProjectRepository : GenericRepository<Project>, IProjectRepository
+    {
+        public ProjectRepository(ProjectsApiDbContext context, ILogger logger) : base(context, logger)
+        {
+        }
+    }
+}
