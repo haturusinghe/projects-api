7745161911c37b5176f2e72d730808a516ff9a31
Maneesha-Maduranga <maneeshamaduranga22@gmail.com>
2023-11-24 09:30:37 +0530

    Add Projects model

---
+++ models/Project.cs
@@ -0,0 +1,11 @@
+﻿namespace Projects_api.models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Revenue { get; set; }
+        public bool isCompleted { get; set; } = false;
+
+    }
+}
