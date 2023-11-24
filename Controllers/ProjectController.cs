c3e0f5cf8e6d75c237b7222c81ba6e94591f1866
Maneesha-Maduranga <maneeshamaduranga22@gmail.com>
2023-11-24 10:26:54 +0530

    Add ProjectController

---
+++ Controllers/ProjectController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Projects_api.models;
+
+namespace Projects_api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProjectController : Controller
+    {
+
+        private readonly ProjectContext _projectcontext;
+        public ProjectController(ProjectContext projectContext)
+        {
+            _projectcontext = projectContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> getAllProjects()
+        {
+            var projects = await _projectcontext.Projects.ToListAsync();
+            return Ok(projects);
+        }
+    }
+}
