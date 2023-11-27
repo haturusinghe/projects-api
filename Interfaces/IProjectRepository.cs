5598b131f5b53d2cb90144402b1000cc680c11f7
haturusinghe <s.haturusinghe99@gmail.com>
2023-11-27 21:25:09 +0530

    added interfaces for repository

---
+++ Interfaces/IProjectRepository.cs
@@ -0,0 +1,10 @@
+﻿using ProjectsAPI.Data.Entities;
+
+namespace ProjectsAPI.Interfaces
+{
+    public interface IProjectRepository : IGenericRepository<Project>
+    {
+        Task<IEnumerable<Project>> GetCompleted();
+        Task<IEnumerable<Project>> GetTopByRevenue();
+    }
+}
