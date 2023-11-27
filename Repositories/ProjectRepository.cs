9618a12b230d0d23415c8b963b8894c969110735
haturusinghe <s.haturusinghe99@gmail.com>
2023-11-27 21:42:21 +0530

    added project repository

---
+++ Repositories/ProjectRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectsAPI.Data;
+using ProjectsAPI.Data.Entities;
+using ProjectsAPI.Interfaces;
+
+namespace ProjectsAPI.Repositories
+{
+    public class ProjectRepository : GenericRepository<Project>, IProjectRepository
+    {
+        public ProjectRepository(ProjectsApiDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<IEnumerable<Project>> GetCompleted()
+        {
+           return await _context.Projects.Where(p => p.IsCompleted == true).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Project>> GetTopByRevenue()
+        {
+            return await _context.Projects.Where(p => p.Revenue > 0).
+                OrderByDescending(p => p.Revenue).Take(3).ToListAsync(); 
+        }
+    }
+}
